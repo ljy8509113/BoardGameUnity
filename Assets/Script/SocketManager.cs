551989912d8f1265cb4b0129ee6be38d811d290d
ljy850113 <ljy8509113@nate.com>
2018-02-28 17:42:28 +0900

    init

---
+++ Assets/Script/SocketManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class SocketManager : MonoBehaviour {
+    
+    public string ip = "192.168.0.8";
+    public const int port = 8895;
+
+    static Socket socket;
+    static IPEndPoint endPoint;
+    
+    void Awake()
+    {
+        
+    }
+
+    public void onSendMessage()
+    {
+        string dataToSend = "send test text";
+        byte[] data = Encoding.Default.GetBytes(dataToSend);
+        
+        
+    }
+
+
+
+}
---
+++ Assets/Script/WebView.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebView : MonoBehaviour {
+
+    private WebViewObject webViewObject;
+    
+    // Use this for initialization 
+
+    void Start()
+    {
+        StartWebView();
+    }
+    
+    // Update is called once per frame 
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Destroy(webViewObject);
+                return;
+            }
+        }
+    }
+
+    public void StartWebView()
+    {
+        string strUrl = "http://192.168.0.8:8080/BoardGame/gameList.do";        
+        webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+
+        webViewObject.Init((msg) => {
+            Debug.Log(string.Format("CallFromJS[{0}]", msg));
+        });
+
+        webViewObject.LoadURL(strUrl);
+        webViewObject.SetVisibility(true);
+        webViewObject.SetMargins(50, 50, 50, 50);
+    }
+}
