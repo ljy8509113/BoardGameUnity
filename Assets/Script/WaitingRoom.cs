a79028ee7b2590cfd0b7550785be100f0db9074d
ljy850113 <ljy8509113@nate.com>
2018-03-15 17:05:12 +0900

    ing

---
+++ Assets/Script/WaitingRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitingRoom : MonoBehaviour {
+
+    public GameObject userItem;
+    public Button buttonReady;
+    public Button buttonCancel;
+    public Text title;
+
+	// Use this for initialization
+	void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onReady()
+    {
+
+    }
+
+    public void onCancel()
+    {
+
+    }
+
+}
---
+++ Assets/Script/WaitingRoomItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaitingRoomItem : MonoBehaviour {
+
+    public Text textNickName;
+    public Text textState;
+    public Button buttonOut;
+    public Plane planBackground;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onOut()
+    {
+
+    }
+    
+}
